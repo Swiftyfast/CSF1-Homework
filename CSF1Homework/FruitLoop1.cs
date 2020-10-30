b6799155a4d767eae212288efb55aa1c18551eb4
John Swift <jdavidswift@gmail.com>
2020-10-30 14:38:16 -0500

    Did the fruit loops warm up.

---
+++ CSF1Homework/FruitLoop1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class FruitLoop1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Fruit Loops Warm Up");
+
+            string[] fruitNames = { "lemon", "pear", "lime", "banana", "orange", "watermelon" };
+            for (int i = 0; i < fruitNames.Length; i++)
+            {
+                Console.WriteLine(fruitNames[i]);
+            }
+        }
+    }
+}
