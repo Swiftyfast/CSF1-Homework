df4e50014fa7e7ce4c877f93327795085754a43b
John Swift <jdavidswift@gmail.com>
2020-10-31 14:16:15 -0500

    Did the fruit loops 2 for loop.

---
+++ CSF1Homework/FruitLoop2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1Homework
+{
+    class FruitLoop2
+    {
+        static void Main()
+        {
+            Console.WriteLine("*********Fruit Loop 2**********");
+
+            string[] fruitNames = { "lemon", "pear", "lime", "banana", "orange", "watermelon" };
+            string periodString = "";
+            for (int i = 0; i < fruitNames.Length; i++)
+            {
+                periodString = periodString + ".";
+                Console.WriteLine(fruitNames[i]);
+                Console.WriteLine(periodString);
+            }
+        }
+    }
+}
